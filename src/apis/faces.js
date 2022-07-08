5cebd76d7ce06bffea1240a8bb748d9962d3018d
Max Erkel <>
2022-07-08 22:25:22 +0200

    implemented getFaces() in faces.js

--- src/apis/faces.js
+++ src/apis/faces.js
@@ -1,6 +1,26 @@
-//Use https://github.com/ozgrozer/100k-faces to get random face from thispersondontexist
+//Use https://github.com/ozgrozer/100k-faces to get random face
 // Gives you a random image
 // https://100k-faces.glitch.me/random-image
 //
 // Gives you a random image URL as JSON
 // https://100k-faces.glitch.me/random-image-url
+
+import axios from 'axios';
+
+async function getFaces(amount) {
+  let response = [];
+  for (let i = 0; i < amount; i++) {
+    try {
+      axios
+        .get('https://100k-faces.glitch.me/random-image-url')
+        .then((resp) => {
+          response.push(resp.data.url);
+        });
+    } catch (err) {
+      console.log('An error has occoured while generating faces', err);
+    }
+  }
+  return response;
+}
+
+export default getFaces;
