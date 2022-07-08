ea5f19098184e1c52a3962df9f1abbd6bef10d6a
Moritz Bantleon <moritz@bantleon.me>
2022-07-08 21:46:38 +0200

    added 11ty and folder structure for generating data

---
+++ src/apis/biography.js
@@ -0,0 +1,3 @@
+// use the data from personalData and a template to generate a biography
+//https://www.frase.io/tools/ai-content-generator/
+// template:
---
+++ src/apis/faces.js
@@ -0,0 +1,6 @@
+//Use https://github.com/ozgrozer/100k-faces to get random face from thispersondontexist
+// Gives you a random image
+// https://100k-faces.glitch.me/random-image
+//
+// Gives you a random image URL as JSON
+// https://100k-faces.glitch.me/random-image-url
