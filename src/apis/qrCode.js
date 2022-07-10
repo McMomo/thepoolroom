29a5a7b8fd5966a05cea5df9fe83088394437659
Max Erkel <>
2022-07-10 21:05:30 +0200

    fixed generation bug

--- src/apis/qrCode.js
+++ src/apis/qrCode.js
@@ -1,21 +1,16 @@
 import QRCode from 'qrcode';
 
-
 //Create QR Code by giving it the id of a dataset. Output file will be saved to /output/qrcodes
 async function createQRCode(id, size, name) {
   const baseURI = 'https://poolroom.neocities.org/';
   // With promises
   await QRCode.toFile(
-    '../../output/qrcodes/' + size + '/' + name + '.png',
+    './../output/qrcodes/' + size + '/' + name + '.png',
     baseURI + id,
     {
       errorCorrectionLevel: 'H',
       width: size,
       height: size,
-    },
-    function (err) {
-      if (err) throw err;
-      console.log('done');
     }
   );
 }
--- src/generateQRCodes.js
+++ src/generateQRCodes.js
@@ -3,43 +3,47 @@ import createQRCode from './apis/qrCode.js';
 
 async function generateQRCodes() {
   const starttime = Date.now();
-  // Use JSON file for storage
-  const file = './src/_data/db.json';
-  const adapter = new JSONFile(file);
-  const db = new Low(adapter);
+  try {
+    // Use JSON file for storage
+    const file = './_data/db.json';
+    const adapter = new JSONFile(file);
+    const db = new Low(adapter);
 
-  // Read data from JSON file, this will set db.data content
-  await db.read();
+    // Read data from JSON file, this will set db.data content
+    await db.read()
+    
+    const size = Object.keys(db.data.persons).length;
+    let persons = db.data.persons;
 
-  const size = Object.keys(db.data.persons).length;
-  let persons = db.data.persons;
-
-  //Kill the process if db is empty
-  if (size <= 0) {
-    return;
-  }
-
-  let nbr = 0;
-  let char = 65; //Unicode A
-
-  for (let i = 0; i < size; i++) {
-    //Generate QR Code name
-    let name = '';
-    if (nbr < 10) {
-      name = String.fromCodePoint(char) + '0' + nbr;
-    } else {
-      name = String.fromCodePoint(char) + '' + nbr;
+    //Kill the process if db is empty
+    if (size <= 0) {
+      return;
     }
 
-    //Create QR Code with the size of 512 & 1024
-    await createQRCode(persons[i].id, 512, name);
-    await createQRCode(persons[i].id, 1024, name);
+    let nbr = 0;
+    let char = 65; //Unicode A
 
-    nbr++;
-    if (nbr == 100) {
-      nbr = 0;
-      char++;
+    for (let i = 0; i < size; i++) {
+      //Generate QR Code name
+      let name = '';
+      if (nbr < 10) {
+        name = String.fromCodePoint(char) + '0' + nbr;
+      } else {
+        name = String.fromCodePoint(char) + '' + nbr;
+      }
+
+      //Create QR Code with the size of 512 & 1024
+      await createQRCode(persons[i].id, 512, name);
+      await createQRCode(persons[i].id, 1024, name);
+
+      nbr++;
+      if (nbr == 100) {
+        nbr = 0;
+        char++;
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
 
   //Print total generation time
